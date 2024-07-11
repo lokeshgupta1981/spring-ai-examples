ff571c34d29bf5551f0b4905afa3a8bfcf32a48c
lokeshgupta1981 <howtodoinjava@gmail.com>
2024-07-11 12:49:37 +0530

    Updated Code

---
+++ ai/text-to-speech/README.md
@@ -0,0 +1,7 @@
+# Spring AI Text-to-Speech (TTS)
+
+In Spring AI, the SpeechModel and StreamingSpeechModel interfaces allow to interact with a Text-to-Speech (TTS) APIs of sopported LLMs such as <em>tts-1</em> and <em>tts-1-hd</em> by OpenAI. and It allows us to convert text transcripts into lifelike spoken audio.
+
+## Related Tutorials
+
+- [Spring AI SpeechModel: Text to Speech Example](https://howtodoinjava.com/spring-ai/tts-text-to-speech/)
--- ai/text-to-speech/src/main/java/com/howtodoinjava/ai/demo/SpeechController.java
+++ ai/text-to-speech/src/main/java/com/howtodoinjava/ai/demo/SpeechController.java
@@ -1,32 +1,66 @@
 package com.howtodoinjava.ai.demo;
 
+import org.springframework.ai.model.ModelOptions;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
 import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
 import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.api.OpenAiAudioApi.SpeechRequest.AudioResponseFormat;
 import org.springframework.ai.openai.audio.speech.SpeechModel;
 import org.springframework.ai.openai.audio.speech.SpeechPrompt;
+import org.springframework.ai.openai.audio.speech.SpeechResponse;
+import org.springframework.ai.openai.audio.speech.StreamingSpeechModel;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
+@CrossOrigin
 class SpeechController {
 
-  private final SpeechModel speechModel;
+  private final OpenAiAudioSpeechModel speechModel;
 
-  SpeechController(SpeechModel speechModel) {
+  SpeechController(OpenAiAudioSpeechModel speechModel) {
     this.speechModel = speechModel;
   }
 
-  @GetMapping("/speech")
-  byte[] speech(@RequestParam(defaultValue = "Speech was missing in the request") String message) {
+  @PostMapping("/speech")
+  byte[] speech(@RequestBody String message) {
+
     return speechModel.call(new SpeechPrompt(message))
       .getResult()
       .getOutput();
   }
 
-  @GetMapping("/speech_v2")
-  byte[] speechWithOpenAiOptions(
-    @RequestParam(defaultValue = "Speech was missing in the request") String message) {
+  @GetMapping("/stream-speech")
+  Flux<byte[]> streamingSpeech(@RequestParam String message) {
+
+    OpenAiAudioSpeechOptions speechOptions = OpenAiAudioSpeechOptions.builder()
+      .withResponseFormat(AudioResponseFormat.OPUS)
+      .build();
+
+    OpenAiAudioApi openAiAudioApi = new OpenAiAudioApi(System.getenv("OPENAI_API_KEY"));
+    StreamingSpeechModel streamingSpeechModel = new OpenAiAudioSpeechModel(openAiAudioApi, speechOptions);
+
+    return streamingSpeechModel.stream(message);
+  }
+
+  @PostMapping("/speech_v2")
+  byte[] speechWithOpenAiOptions(@RequestBody String message) {
+
+    /*OpenAiAudioSpeechOptions speechOptions = OpenAiAudioSpeechOptions.builder()
+      .withModel(OpenAiAudioApi.TtsModel.TTS_1.getValue())
+      .withResponseFormat(AudioResponseFormat.MP3)
+      .withVoice(OpenAiAudioApi.SpeechRequest.Voice.ALLOY)
+      .withSpeed(1.0f)
+      .build();
+
+    var openAiAudioApi = new OpenAiAudioApi(System.getenv("OPENAI_API_KEY"));
+    SpeechModel openAiAudioSpeechModel = new OpenAiAudioSpeechModel(openAiAudioApi, speechOptions);*/
+
     var speechResponse = speechModel
       .call(new SpeechPrompt(message, OpenAiAudioSpeechOptions.builder()
         .withModel("tts-1")
