260792eb7954948b2aa3dfbf1a5f9b71f2c92b5f
lokeshgupta1981 <howtodoinjava@gmail.com>
2024-07-19 17:55:36 +0530

    Refactor

--- ai/similarity-search/src/main/java/com/howtodoinjava/ai/demo/SemanticSearchService.java
+++ ai/similarity-search/src/main/java/com/howtodoinjava/ai/demo/SemanticSearchService.java
@@ -1,8 +1,14 @@
 package com.howtodoinjava.ai.demo;
 
 import java.util.List;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.filter.Filter;
+import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
+import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder.Op;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +21,16 @@ public class SemanticSearchService {
   }
 
   List<LogRecord> semanticSearch(String query) {
+
+    // With default parameters
+    SearchRequest searchRequest = SearchRequest.query(query);
+
+    // Customize parameters
+    /* SearchRequest searchRequest = SearchRequest.defaults()
+      .withTopK(10)
+      .withFilterExpression(new FilterExpressionBuilder().eq("LEVEL", "INFO").build())
+      .withSimilarityThreshold(0.5);*/
+
     return vectorStore.similaritySearch(SearchRequest.query(query))
       .stream()
       .map(document -> new LogRecord(document.getContent()))
