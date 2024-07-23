264a75e86f6e8d89757db40b0bc2a590964d3353
lokeshgupta1981 <howtodoinjava@gmail.com>
2024-07-23 16:53:36 +0530

    Refactor

---
+++ ai/function-callback/README.md
@@ -0,0 +1,5 @@
+# Function Calling with Spring AI
+
+## Related Tutorials
+
+- [Spring AI Function Calling with Spring Boot](https://howtodoinjava.com/spring-ai/function-calling-example/)
--- ai/function-callback/src/main/java/com/howtodoinjava/ai/demo/ChatService.java
+++ ai/function-callback/src/main/java/com/howtodoinjava/ai/demo/ChatService.java
@@ -1,26 +1,48 @@
 package com.howtodoinjava.ai.demo;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
 
   private final ChatClient chatClient;
+  private final ChatModel chatModel;
 
-  ChatService(ChatClient.Builder chatClientBuilder) {
+
+  ChatService(ChatClient.Builder chatClientBuilder,
+    ChatModel chatModel) {
     this.chatClient = chatClientBuilder.build();
+    this.chatModel = chatModel;
   }
 
   String getPriceByStockName(String stockName) {
     var userPromptTemplate = "Get the latest price for {stockName}.";
-    return chatClient.prompt()
+
+    PromptTemplate promptTemplate = new PromptTemplate(userPromptTemplate);
+    Message message = promptTemplate.createMessage(Map.of("stockName", stockName));
+
+    ChatResponse response = chatModel.call(
+      new Prompt(List.of(message), OpenAiChatOptions.builder()
+        .withFunction("priceByStockNameFunction").build()));
+
+    return response.getResult().getOutput().getContent();
+
+    /*return chatClient.prompt()
       .user(userSpec -> userSpec
         .text(userPromptTemplate)
         .param("stockName", stockName)
       )
       .functions("priceByStockNameFunction")
       .call()
-      .content();
+      .content();*/
   }
 }
